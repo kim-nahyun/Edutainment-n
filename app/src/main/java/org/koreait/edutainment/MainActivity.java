e75d3977c80e52bba7b2b0523e3d53652eb37e5d
11mon <11monqueen11@naver.com>
2023-11-06 10:59:26 +0900

    수정

--- app/src/main/java/org/koreait/edutainment/MainActivity.java
+++ app/src/main/java/org/koreait/edutainment/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
---
+++ app/src/main/java/org/koreait/edutainment/SpeakActivity.java
@@ -0,0 +1,5 @@
+package org.koreait.edutainment;
+
+public class SpeakActivity {
+
+}
